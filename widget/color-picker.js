6831fbd900a66d8344205fdd3b45e1863abad308
VisualSj <itharbors@outlook.com>
2015-11-05 17:25:16 +0800

    Color allows the range to change to 0-255

--- widget/color-picker.js
+++ widget/color-picker.js
@@ -7,7 +7,7 @@ Editor.registerElement({
                     r: 255,
                     g: 255,
                     b: 255,
-                    a: 1,
+                    a: 255,
                 };
             },
             notify: true,
@@ -33,10 +33,10 @@ Editor.registerElement({
         cssRGB = chroma(cssRGB.r, cssRGB.g, cssRGB.b).css('rgb');
         this.$.colorCtrl.style.backgroundColor = cssRGB;
         this.$.opacityCtrl.style.backgroundColor = cssRGB;
-        this.$.opacityHandle.style.top = (1.0 - this.value.a) * 100 + '%';
-        this.$.hueHandle.style.top = (1.0 - this.hsv.h) * 100 + '%';
+        this.$.opacityHandle.style.top = (255 - this.value.a) / 255 * 100 + '%';
+        this.$.hueHandle.style.top = (1 - this.hsv.h) * 100 + '%';
         this.$.colorHandle.style.left = this.hsv.s * 100 + '%';
-        this.$.colorHandle.style.top = (1.0 - this.hsv.v) * 100 + '%';
+        this.$.colorHandle.style.top = (1 - this.hsv.v) * 100 + '%';
     },
 
     _hueCtrlMouseDownAction: function ( event ) {
@@ -130,9 +130,9 @@ Editor.registerElement({
         var updateMouseMove = function (event) {
             event.stopPropagation();
 
-            var offsetY = (event.clientY - mouseDownY)/this.$.opacityCtrl.clientHeight;
-            offsetY = Math.max( Math.min( offsetY, 1.0 ), 0.0 );
-            this.set( 'value.a', parseFloat((1.0 - offsetY).toFixed(2))  );
+            var offsetY = (event.clientY - mouseDownY)/this.$.opacityCtrl.clientHeight*255;
+            offsetY = Math.max( Math.min( offsetY, 255 ), 0 );
+            this.set( 'value.a', parseInt((255 - offsetY))  );
             this._repaint();
         };
         updateMouseMove.call(this,event);
